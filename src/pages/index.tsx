be2161795ac214d96aa44b4b12e17d7010270cde
Victor Roberto <robertovictor0702@gmail.com>
2021-12-02 14:01:49 -0300

    subscribe button add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import styles from './home.module.scss';
 
+import { SubscribeButton } from '../components/subscribeButton';
+
 export default function Home() {
   return (
     <>
@@ -13,6 +15,7 @@ export default function Home() {
               Get access to all the publications <br />
               <span>for $9.90 month</span>
             </p>
+            <SubscribeButton/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
